281c02435a814e62d5f30d28bb876967b0a61637
EricJhang <artopllting@gmail.com>
2018-02-27 17:22:50 +0800

    add repose text

--- app.js
+++ app.js
@@ -55,11 +55,11 @@ function handleMessage(sender_psid, received_message) {
     let response;
 
   // Check if the message contains text
-  if (received_message.text) {    
+  if (received_message !="") {    
 
     // Create the payload for a basic text message
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      "text": received_message
     }
   }  
   
